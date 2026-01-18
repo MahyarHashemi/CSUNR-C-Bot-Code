e541defc5afb3449cdc2bf5a871caae4147e578e
MahyarHashemi <88331830+MahyarHashemi@users.noreply.github.com>
2026-01-17 18:35:30 -0800

    Changed include for new pneumatic control

--- include/EZ-Template/pneumatics.hpp
+++ include/EZ-Template/pneumatics.hpp
@@ -2,6 +2,8 @@
 
 extern bool rake_state;
 extern bool angle_state;
+extern bool ball_state;
+extern bool descore_state;
 
 ////////////////////Pneumatics Auton///////////////////////
 
@@ -15,6 +17,9 @@ void angle_close();
 
 void ball_close();
 void ball_open();
+
+void descore_open();
+void descore_close();
 
 //////////////////////Pneumatic Control/////////////////
 void pneumatic_control();
